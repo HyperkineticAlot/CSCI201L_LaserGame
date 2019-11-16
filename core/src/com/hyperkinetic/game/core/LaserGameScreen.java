9085cb058d275b7c0705710e6084466609c7d5e3
Amber Guo <yig152@ucsd.edu>
2019-11-16 13:02:01 -0800

    Add game title picture and temp background picture; changed the structure of main class. LaserGameCopy.java served as reference; Pending animated background research and settings page configuration

---
+++ core/src/com/hyperkinetic/game/core/LaserGameScreen.java
@@ -0,0 +1,86 @@
+package com.hyperkinetic.game.core;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.hyperkinetic.game.board.AbstractGameBoard;
+import com.hyperkinetic.game.board.StandardBoard;
+
+public class LaserGameScreen implements Screen {
+
+    /**
+     * The {@link SpriteBatch} object responsible for rendering textures onto the game canvas.
+     */
+    private static SpriteBatch batch;
+    /**
+     * The game board that tracks current configuration and player movements.
+     */
+    private AbstractGameBoard board;
+    /**
+     * Tracks all objects (such as Textures) that implement {@link Disposable} and need to be freed.
+     */
+    private static Array<Disposable> disposables = new Array<>();
+
+    private Stage stage;
+    private Game game;
+
+    public LaserGameScreen(Game aGame) {
+        game = aGame;
+        stage = new Stage(new ScreenViewport());
+        board = new StandardBoard();
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(100, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        board.render(batch);
+
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        for(Disposable d : disposables)
+        {
+            d.dispose();
+        }
+        disposables.clear();
+    }
+}
---
+++ core/src/com/hyperkinetic/game/core/MainMenuScreen.java
@@ -0,0 +1,157 @@
+package com.hyperkinetic.game.core;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import javax.xml.soap.Text;
+
+public class MainMenuScreen  extends InputAdapter implements Screen {
+    LaserGame game;
+    private Stage stage;
+    private SpriteBatch batch;
+    private Label outputLabel;
+    private Texture backgroundPic;
+    private Texture titlePic;
+
+    public MainMenuScreen (final LaserGame game) {
+        // constructor
+        this.game = game;
+
+        stage = new Stage(new ScreenViewport());
+        batch = new SpriteBatch();
+
+        backgroundPic = new Texture(Gdx.files.internal("tempBackground.png"));
+        titlePic = new Texture(Gdx.files.internal("LaserGameTitle.png"));
+
+        Skin neon = new Skin(Gdx.files.internal("skin/neon-ui.json"));
+
+        Button login = new TextButton("LOG IN", neon);
+        login.setSize(200,100);
+        login.setPosition(1920/2 - 100, 1280/2);
+        login.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new LaserGameScreen(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        stage.addActor(login);
+
+
+        Button guest = new TextButton("GUEST", neon);
+        guest.setSize(200,100);
+        guest.setPosition(1920/2 - 100, 1280/2 + 100);
+        guest.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new LaserGameScreen(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        stage.addActor(guest);
+
+
+        Button quit = new TextButton("QUIT", neon);
+        quit.setSize(200,100);
+        quit.setPosition(1920/2 - 100, 1280/2 - 200);
+        quit.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.exit();
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        stage.addActor(quit);
+
+
+        Button settings = new TextButton("SETTINGS", neon);
+        settings.setSize(200,100);
+        settings.setPosition(1920/2 - 100, 1280/2 - 100);
+        settings.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.exit();
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        stage.addActor(settings);
+
+
+        // test propose
+        outputLabel = new Label("Press a Button",neon);
+        outputLabel.setSize(100,100);
+        outputLabel.setPosition(0,500);
+        outputLabel.setAlignment(Align.center);
+        // test propose
+
+        stage.addActor(outputLabel);
+    }
+
+
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(255,255,255, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.getBatch().begin();
+        stage.getBatch().draw(backgroundPic, 0, 0, 1920, 1280);
+        stage.getBatch().draw(titlePic, 1920 / 2 - 958/ 2 , 1000);
+        stage.getBatch().end();
+
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
