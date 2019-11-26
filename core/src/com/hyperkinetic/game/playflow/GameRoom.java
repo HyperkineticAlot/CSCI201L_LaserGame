cf774c6fd9860a1c507b00647232131286a3b8c9
DESKTOP-0QL4009\DELL <jiefan_y@yahoo.com>
2019-11-25 17:55:31 -0800

    get rid of ready/copy

--- core/src/com/hyperkinetic/game/playflow/GameRoom.java
+++ core/src/com/hyperkinetic/game/playflow/GameRoom.java
@@ -173,19 +173,6 @@ public class GameRoom {
         }
     }
 
-    private void handleReady(GameMessage message){
-        if(message.getMessageType()!=messageType.READY) return;
-
-        String playerID = message.playerID;
-        GameMessage copy = new GameMessage(messageType.COPY);
-        copy.playerID = playerID;
-        if(playerID.equals(aThread.getPlayerID())){
-            aThread.sendMessage(copy);
-        } else {
-            bThread.sendMessage(copy);
-        }
-    }
-
     /**
      * Read the message from the server.
      * @param message the message from the server
@@ -194,8 +181,6 @@ public class GameRoom {
     {
         if(message.getMessageType()==messageType.PLAYER_MOVE){
             handleMoveAttempt(message);
-        } else if(message.getMessageType()==messageType.READY){
-            handleReady(message);
         } else if(message.getMessageType()==messageType.STATS_REQUEST){
             GameMessage gm = gs.queryDatabase(message);
             broadcast(gm);
